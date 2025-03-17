cf2038922858f581f5695c5fb3b63e44fb48fd65
mehul79 <mehulrgupta@gmail.com>
2025-03-17 17:34:00 +0530

    post endpoint added

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.post("/user", (req,res) => {
+  const body = req.body;
+  res.json({msg: "User created", body});
+});
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+
 
 export default app;
